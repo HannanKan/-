c2af0d75a2bb22c145305a072941332b0a519bcd
Hannankan <hannankan@foxmail.com>
2021-04-22 23:35:28 +0800

    添加自动下载

--- download.js
+++ download.js
@@ -28,6 +28,8 @@ for (node of l){
 
 /** 第3版**/
 /**
+ * 设置： chrome设置打开标签自动弹出开发者模式窗口https://www.jianshu.com/p/fd5ff7a19346
+ * 
  * 开发说明：
  * 1. querySelector找到iframe之后需要 .contentDocument 返回里面的 html 文档，才能进一步用querySelector查找元素
  * 2. 这次的html页面布局发生改变，html 文档里面是一个iframe，iframe里面的文档又内嵌了几个个iframe，每个iframe里面是视频、PPT或者其他内容
@@ -39,9 +41,23 @@ for (node of l){
  * 3. 在console里面输入以下代码，会输出1个或几个网址
  * 4. 逐个点开网址，浏览器里显示一连串英文，找到“download”后面网址，输入到浏览器中即可下载
  */
+//在课程页面
 let l=document.querySelector("iframe").contentDocument.querySelectorAll("iframe")
 for (node of l){
     let objectid = node.getAttribute("objectid")
     let url = `https://mooc1-2.chaoxing.com/ananas/status/${objectid}`
-    console.log(url)
+    //console.log(url)
+    window.open(url, '_blank').location;   //打开所有可能的资源的标签
 }
+
+//在已经打开的资源的标签页面的开发者工具里面的console中
+let jsonStr=document.querySelector("pre").innerText
+let jsonObj=JSON.parse(jsonStr)
+//制造https 链接 替代http链接
+downloadlink=jsonObj.download
+downloadlink=downloadlink.slice(0,4)+"s"+downloadlink.slice(4)
+        //参考链接：https://www.jianshu.com/p/52da82aa29d3
+let ele = document.createElement('a');
+ele.setAttribute('href',downloadlink); //设置下载文件的url地址
+ele.setAttribute('download' , jsonObj.filename);//用于设置下载文件的文件名
+ele.click();
